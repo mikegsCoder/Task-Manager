81aa8bf71421b6aee4a2b6c4c8fa59c4a5bdc8fb
mikegsCoder <mikegsid@gmail.com>
2023-04-26 18:12:30 +0300

    Add DatabaseConstants

---
+++ TaskManager/TaskManager.Infrastructure/Constants/DatabaseConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Infrastructure.Constants
+{
+    internal static class DatabaseConstants
+    {
+        // User model:
+        public const int User_Username_MaxLength = 15;
+        public const int User_FirstName_MaxLength = 15;
+        public const int User_LastName_MaxLength = 15;
+
+        // UserTask model:
+        public const int UserTask_Description_MaxLength = 75;
+
+        // Reamrk model:
+        public const int Remark_Content_MaxLength = 75;
+
+        // Category model:
+        public const int Category_Name_MaxLength = 10;
+
+        // Status model:
+        public const int Status_Name_MaxLength = 10;
+    }
+}
