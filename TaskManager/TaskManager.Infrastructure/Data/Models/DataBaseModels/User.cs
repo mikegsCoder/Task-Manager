8a6d5561c4ea5d661ca3cfd46a08d6ced65c2ff8
mikegsCoder <mikegsid@gmail.com>
2023-04-26 18:14:54 +0300

    Add User db model

---
+++ TaskManager/TaskManager.Infrastructure/Data/Models/DataBaseModels/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManager.Infrastructure.Constants;
+
+namespace TaskManager.Infrastructure.Data.Models.DataBaseModels
+{
+    public class User
+    {
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+
+            Tasks = new List<UserTask>();
+        }
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(DatabaseConstants.User_Username_MaxLength)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        public string PasswordHash { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DatabaseConstants.User_FirstName_MaxLength)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DatabaseConstants.User_LastName_MaxLength)]
+        public string LastName { get; set; } = null!;
+
+        public virtual ICollection<UserTask> Tasks { get; set; }
+    }
+}
