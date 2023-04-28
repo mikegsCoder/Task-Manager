df33815654939452d9155b59a5f840b9ca400fe7
mikegsCoder <mikegsid@gmail.com>
2023-04-28 06:42:34 +0300

    Add Db models into ApplicationDbContext

--- TaskManager/TaskManager.Infrastructure/Data/ApplicationDbContext.cs
+++ TaskManager/TaskManager.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Infrastructure.Data.Models.DataBaseModels;
 
 namespace TaskManager.Infrastructure.Data
 {
@@ -8,6 +9,13 @@ namespace TaskManager.Infrastructure.Data
                 : base(options)
         {
         }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserTask> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Remark> Remarks { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=.;Database=TaskManager;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true");
