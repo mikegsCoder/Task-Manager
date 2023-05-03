286c4847d506f95e1622bc2d9b2ad585d124dcbd
mikegsCoder <mikegsid@gmail.com>
2023-05-03 17:38:29 +0300

    Add UserController

---
+++ TaskManager/TaskManager.WPF/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TaskManager.Core.Constants;
+using TaskManager.Core.Services.UserService;
+using TaskManager.Core.ViewModels.User;
+using TaskManager.WPF.DataContexts;
+using static TaskManager.WPF.Windows.MessageBoxes.MessageBoxes;
+
+namespace TaskManager.WPF.Controllers
+{
+    public class UserController
+    {
+        private readonly MainWindowContext context;
+        private readonly IUserService userService;
+
+        public UserController(
+            IUserService _userService,
+            MainWindowContext _context)
+        {
+            userService = _userService;
+            context = _context;
+        }
+    }
+}
