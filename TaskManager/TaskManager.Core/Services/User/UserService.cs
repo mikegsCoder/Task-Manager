537578d44907de9c464afdf9a3e39651d69e69bb
mikegsCoder <mikegsid@gmail.com>
2023-05-04 16:38:29 +0300

    Add ComputeHash method

--- TaskManager/TaskManager.Core/Services/User/UserService.cs
+++ TaskManager/TaskManager.Core/Services/User/UserService.cs
@@ -15,5 +15,24 @@ namespace TaskManager.Core.Services.UserService
         {
             db = _db;
         }
+
+        private string ComputeHash(string password)
+        {
+            var bytes = Encoding.UTF8.GetBytes(password);
+
+            using (var hash = SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+
+                var hashedInputStringBuilder = new StringBuilder(128);
+
+                foreach (var b in hashedInputBytes)
+                {
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                }
+
+                return hashedInputStringBuilder.ToString();
+            }
+        }
     }
 }
