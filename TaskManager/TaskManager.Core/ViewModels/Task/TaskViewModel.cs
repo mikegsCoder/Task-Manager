bc1ca9ee8d8547844034c4fa039c41efbf12e887
mikegsCoder <mikegsid@gmail.com>
2023-05-04 16:37:05 +0300

    Add TaskViewModel

---
+++ TaskManager/TaskManager.Core/ViewModels/Task/TaskViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskManager.Core.ViewModels.Task
+{
+    public class TaskViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string? Category { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? CreatedOn { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public string? FinishedOn { get; set; }
+    }
+}
