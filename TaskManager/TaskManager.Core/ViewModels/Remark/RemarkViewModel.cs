0edb8f1f61d981041fd67f317a1613f6580f71c2
mikegsCoder <mikegsid@gmail.com>
2023-05-04 16:39:35 +0300

    Add RemarkViewModel

---
+++ TaskManager/TaskManager.Core/ViewModels/Remark/RemarkViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Core.ViewModels.Remark
+{
+    public class RemarkViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string TaskId { get; set; } = null!;
+
+        public string? Content { get; set; }
+
+        public string? CreatedOn { get; set; }
+    }
+}
