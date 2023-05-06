dd590ffa21dc3fd652a78c817f42705bba4c1038
mikegsCoder <mikegsid@gmail.com>
2023-05-06 09:21:34 +0300

    Add Messages

---
+++ TaskManager/TaskManager.Core/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Core.Constants
+{
+    public static class Messages
+    {
+        // UserController:
+        // UserController Login:
+        public const string Login_Input_Error_Msg = "Both fields are required.";
+        public const string Login_Data_Error_Msg = "Invalid Username or Password.";
+        public const string Login_Welcome_Title_Msg = "Welcome again!";
+        public const string Login_Welcome_Text_Msg = "Welcome {0} {1}.";
+    }
+}
--- TaskManager/TaskManager.WPF/Controllers/UserController.cs
+++ TaskManager/TaskManager.WPF/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using TaskManager.Core.Constants;
 using TaskManager.Core.Services.UserService;
 using TaskManager.Core.ViewModels.User;
 using TaskManager.WPF.DataContexts;
