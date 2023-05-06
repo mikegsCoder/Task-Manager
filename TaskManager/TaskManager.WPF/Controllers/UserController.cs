f4a2d1afdae64741d72f87e965f530dd6efb8bdf
mikegsCoder <mikegsid@gmail.com>
2023-05-06 09:18:55 +0300

    Add LoginAsync method

--- TaskManager/TaskManager.WPF/Controllers/UserController.cs
+++ TaskManager/TaskManager.WPF/Controllers/UserController.cs
@@ -23,5 +23,51 @@ namespace TaskManager.WPF.Controllers
             userService = _userService;
             context = _context;
         }
+
+        public async Task LoginAsync()
+        {
+            LoginWindow loginWindow = new LoginWindow();
+
+            if (loginWindow.ShowDialog() == false)
+            {
+                return;
+            }
+
+            string username = loginWindow.Username.Text.Trim();
+            string password = loginWindow.Password.Password.Trim();
+
+            if (string.IsNullOrEmpty(username)
+                || string.IsNullOrEmpty(password))
+            {
+                ShowInvalidInput(Messages.Login_Input_Error_Msg);
+
+                return;
+            }
+
+            try
+            {
+                context.user = await userService.GetUserAsync(username, password);
+
+                if (context.user == null)
+                {
+                    ShowInvalidInput(Messages.Login_Data_Error_Msg);
+                }
+                else
+                {
+                    context.HasUser = true;
+                    context.ShowLoginBtn = false;
+
+                    ShowGreeting(
+                        string.Format(Messages.Login_Welcome_Text_Msg,
+                            context.user.FirstName,
+                            context.user.LastName),
+                        Messages.Login_Welcome_Title_Msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
     }
 }
