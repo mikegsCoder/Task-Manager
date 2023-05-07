292f263bd25c21e02f92dfd0b2552e3cff841b6f
mikegsCoder <mikegsid@gmail.com>
2023-05-07 15:59:41 +0300

    Add UserController Register messages

--- TaskManager/TaskManager.Core/Constants/Messages.cs
+++ TaskManager/TaskManager.Core/Constants/Messages.cs
@@ -9,6 +9,15 @@ namespace TaskManager.Core.Constants
     public static class Messages
     {
         // UserController:
+        // UserController Register:
+        public const string First_Name_Error_Msg = "First Name should be between {0} and {1} characters long.";
+        public const string Last_Name_Error_Msg = "Last Name should be between {0} and {1} characters long.";
+        public const string Username_Error_Msg = "Username should be between {0} and {1} characters long.";
+        public const string Password_Error_Msg = "Password should be between {0} and {1} characters long.";
+        public const string Repass_Error_Msg = "Passwords don't match.";
+        public const string Input_Error_Msg = "Invalid Input!";
+        public const string Username_NotAvailable_Error_Msg = "Username is not available.";
+        public const string Registration_Success_Msg = "Successful registration.";
         // UserController Login:
         public const string Login_Input_Error_Msg = "Both fields are required.";
         public const string Login_Data_Error_Msg = "Invalid Username or Password.";
