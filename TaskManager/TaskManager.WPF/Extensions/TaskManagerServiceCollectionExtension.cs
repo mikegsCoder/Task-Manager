e19cd8f8099ea744a66abc8600d171817139b51a
mikegsCoder <mikegsid@gmail.com>
2023-05-09 19:40:40 +0300

    Add TaskController into ServiceCollection

--- TaskManager/TaskManager.WPF/Extensions/TaskManagerServiceCollectionExtension.cs
+++ TaskManager/TaskManager.WPF/Extensions/TaskManagerServiceCollectionExtension.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<App>();
             services.AddScoped<MainWindow>();
             services.AddScoped<UserController>();
+            services.AddScoped<TaskController>();
             services.AddScoped<MainWindowContext>();
             services.AddDbContext<ApplicationDbContext>();
             services.AddScoped(typeof(IUserService), typeof(UserService));
