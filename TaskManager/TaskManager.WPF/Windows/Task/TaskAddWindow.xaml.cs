38c014492abd9d56d71a4d070cc35493dac9ffcc
mikegsCoder <mikegsid@gmail.com>
2023-05-13 11:59:25 +0300

    Add TaskAddWindow

---
+++ TaskManager/TaskManager.WPF/Windows/Task/TaskAddWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskManager.WPF.Windows.Task
+{
+    /// <summary>
+    /// Interaction logic for TaskAddWindow.xaml
+    /// </summary>
+    public partial class TaskAddWindow : Window
+    {
+        public string category;    
+
+        public TaskAddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed = (RadioButton)sender;
+
+            category = pressed.Content.ToString();
+        }
+    }
+}
