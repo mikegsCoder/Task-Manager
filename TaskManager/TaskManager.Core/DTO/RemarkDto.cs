b1b7a454b5f656df61d8e7bc9d0d5838777218de
mikegsCoder <mikegsid@gmail.com>
2023-05-21 17:07:11 +0300

    Add RemarkDto

---
+++ TaskManager/TaskManager.Core/DTO/RemarkDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Core.DTO
+{
+    public class RemarkDto
+    {
+        public string Content { get; set; } = null!;
+
+        public string CreatedOn { get; set; } = null!;
+    }
+}
