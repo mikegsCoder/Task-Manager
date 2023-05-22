fc4dbabc1dacd3b7374f0787cdcf6c03b18052a5
mikegsCoder <mikegsid@gmail.com>
2023-05-22 17:44:12 +0300

    Add FileService

---
+++ TaskManager/TaskManager.Core/Services/File/FileService.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using TaskManager.Core.DTO;
+using TaskManager.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Core.Services.File
+{
+    public class FileService : IFileService
+    {
+        private readonly ApplicationDbContext db;
+
+        public FileService(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public async void ExportTasksAsync(string userId, string path, string format)
+        {
+           
+        }
+    }
+}
