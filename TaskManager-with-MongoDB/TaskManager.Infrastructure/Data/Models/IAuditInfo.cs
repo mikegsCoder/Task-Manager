998efc918606c68573a5a00e61eadfaa37387582
mikegsCoder <mikegsid@gmail.com>
2023-06-15 12:14:54 +0300

    Add IAuditInfo and IDeletableEntity interfaces

---
+++ TaskManager-with-MongoDB/TaskManager.Infrastructure/Data/Models/IAuditInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Infrastructure.Data.Models
+{
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+
+        bool IsFinished { get; set; }
+
+        DateTime? FinishedOn { get; set; }
+    }
+}
---
+++ TaskManager-with-MongoDB/TaskManager.Infrastructure/Data/Models/IDeletableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Infrastructure.Data.Models
+{
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
