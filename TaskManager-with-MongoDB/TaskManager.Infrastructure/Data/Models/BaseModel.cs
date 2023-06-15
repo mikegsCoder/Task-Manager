0052107e66cc5853a354a25371be4fdaae838f76
mikegsCoder <mikegsid@gmail.com>
2023-06-15 12:16:41 +0300

    Add BaseModel

---
+++ TaskManager-with-MongoDB/TaskManager.Infrastructure/Data/Models/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Infrastructure.Data.Models
+{
+    public abstract class BaseModel<TKey> : IAuditInfo
+    {
+        [BsonId]
+        public TKey Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public DateTime? FinishedOn { get; set; }
+    }
+}
