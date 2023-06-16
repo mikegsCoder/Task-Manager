f6cb619c547a380a094b2644f5e4aa042d328fcf
mikegsCoder <mikegsid@gmail.com>
2023-06-16 10:51:45 +0300

    Add UserTask database model

---
+++ TaskManager-with-MongoDB/TaskManager.Infrastructure/Data/Models/DataBaseModels/UserTask.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Infrastructure.Data.Models.DataBaseModels
+{
+    public class UserTask : BaseDeletableModel<ObjectId>
+    {
+        public ObjectId CategoryId { get; set; }
+
+        public ObjectId StatusId { get; set; }
+
+        public ObjectId UserId { get; set; }
+
+        public string Description { get; set; } = null!;
+    }
+}
