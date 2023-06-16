4f9c7b939d9858dda4674bfb908254bb560b64a1
mikegsCoder <mikegsid@gmail.com>
2023-06-16 10:52:55 +0300

    Add User database model

---
+++ TaskManager-with-MongoDB/TaskManager.Infrastructure/Data/Models/DataBaseModels/User.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Infrastructure.Data.Models.DataBaseModels
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+    }
+}
