fc6b29a98b6c9e4dfb787c84de7aefc167a56650
mikegsCoder <mikegsid@gmail.com>
2023-06-16 10:50:19 +0300

    Add Remark database model

---
+++ TaskManager-with-MongoDB/TaskManager.Infrastructure/Data/Models/DataBaseModels/Remark.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Infrastructure.Data.Models.DataBaseModels
+{
+    public class Remark : BaseDeletableModel<ObjectId>
+    {
+        public string Content { get; set; } = null!;
+
+        public ObjectId TaskId { get; set; }
+    }
+}
