393f04667e3ab4ddcf4602bfc9ee92042946fe88
mikegsCoder <mikegsid@gmail.com>
2023-06-16 10:48:57 +0300

    Add Status detabase model

---
+++ TaskManager-with-MongoDB/TaskManager.Infrastructure/Data/Models/DataBaseModels/Status.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Infrastructure.Data.Models.DataBaseModels
+{
+    public class Status
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
