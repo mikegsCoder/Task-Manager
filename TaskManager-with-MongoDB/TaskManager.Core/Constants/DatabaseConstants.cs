e4a1beea5d34c40845595ca75a40f57138c4629a
mikegsCoder <mikegsid@gmail.com>
2023-06-17 12:28:42 +0300

    Add DatabaseConstants

---
+++ TaskManager-with-MongoDB/TaskManager.Core/Constants/DatabaseConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Core.Constants
+{
+    public static class DatabaseConstants
+    {
+        // Database name:
+        public const string DatabaseName = "taskManager";
+
+        // DB collections names:
+        public const string CategoryCollectionName = "categories";
+        public const string StatusCollectionName = "statuses";
+        public const string UserCollectionName = "users";
+        public const string TaskCollectionName = "tasks";
+        public const string RemarkCollectionName = "remarks";
+    }
+}
