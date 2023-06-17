a21c16baefe14f5cbf85c641dfc7be0184ecaddc
mikegsCoder <mikegsid@gmail.com>
2023-06-17 12:34:49 +0300

    Add MessageBoxes

---
+++ TaskManager-with-MongoDB/TaskManager.WPF/Windows/MessageBoxes/MessageBoxes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TaskManager.WPF.Windows.MessageBoxes
+{
+    public static class MessageBoxes
+    {
+        public static void ShowInvalidInput(string message)
+        {
+            MessageBox.Show(
+                message,
+                "Invalid Input",
+                MessageBoxButton.OK,
+                MessageBoxImage.Exclamation);
+        }
+
+        public static void ShowError(string message)
+        {
+            MessageBox.Show(
+                message,
+                "Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+
+        public static void ShowSuccess(string message)
+        {
+            MessageBox.Show(
+                message,
+                "Success",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        public static void ShowGreeting(string message, string title)
+        {
+            MessageBox.Show(
+                message,
+                title,
+                MessageBoxButton.OK,
+                MessageBoxImage.None);
+        }
+    }
+}
+
