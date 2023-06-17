0c8514e4a513ec20b726f8ba78f9c7471b76d55c
mikegsCoder <mikegsid@gmail.com>
2023-06-17 12:30:56 +0300

    Add UserService

---
+++ TaskManager-with-MongoDB/TaskManager.Core/Services/User/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+using MongoDB.Driver;
+using TaskManager.Core.Constants;
+using TaskManager.Infrastructure.Data.Models.DataBaseModels;
+
+namespace TaskManager.Core.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IMongoClient client;
+        private readonly IMongoDatabase db;
+        private IMongoCollection<User> userCollection;
+
+        public UserService(IMongoClient _client)
+        {
+            client = _client;
+
+            db = client.GetDatabase(DatabaseConstants.DatabaseName);
+
+            userCollection = db.GetCollection<User>(DatabaseConstants.UserCollectionName);
+        }
+
+        public async Task<bool> IsUsernameAvailableAsync(string username)
+        {
+            return await userCollection.Find(x => x.Username == username).FirstOrDefaultAsync() == null;
+        }
+    }
+}
