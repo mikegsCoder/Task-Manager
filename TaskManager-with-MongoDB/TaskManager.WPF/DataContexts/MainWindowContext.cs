fd60dc99942be905cecd0066ed0cbcafcab63816
mikegsCoder <mikegsid@gmail.com>
2023-06-18 11:45:47 +0300

    Add user and tasks into MainWindowContext

--- TaskManager-with-MongoDB/TaskManager.WPF/DataContexts/MainWindowContext.cs
+++ TaskManager-with-MongoDB/TaskManager.WPF/DataContexts/MainWindowContext.cs
@@ -4,12 +4,17 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Core.ViewModels.Task;
+using TaskManager.Core.ViewModels.User;
 
 namespace TaskManager.WPF.DataContexts
 {
     public class MainWindowContext : INotifyPropertyChanged
     {
         private bool hasUser = false;
+
+        public List<TaskViewModel> tasks;
+        public UserViewModel user;
 
         public bool HasUser
         {
@@ -21,6 +26,12 @@ namespace TaskManager.WPF.DataContexts
         {
             get { return !hasUser; }
             set { NotifyPropertyChanged("ShowLoginBtn"); }
+        }
+
+        public bool NoTasks
+        {
+            get { return (tasks != null && tasks.Count == 0); }
+            set { NotifyPropertyChanged("NoTasks"); }
         }
 
         public void NotifyPropertyChanged(string info)
