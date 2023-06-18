a1bb93b966068dacbc5575500e5b68ca8a4ac9c3
mikegsCoder <mikegsid@gmail.com>
2023-06-18 11:48:07 +0300

    Add ComputeHash method

--- TaskManager-with-MongoDB/TaskManager.Core/Services/User/UserService.cs
+++ TaskManager-with-MongoDB/TaskManager.Core/Services/User/UserService.cs
@@ -26,5 +26,24 @@ namespace TaskManager.Core.Services.UserService
         {
             return await userCollection.Find(x => x.Username == username).FirstOrDefaultAsync() == null;
         }
+
+        private string ComputeHash(string password)
+        {
+            var bytes = Encoding.UTF8.GetBytes(password);
+
+            using (var hash = SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+
+                var hashedInputStringBuilder = new StringBuilder(128);
+
+                foreach (var b in hashedInputBytes)
+                {
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                }
+
+                return hashedInputStringBuilder.ToString();
+            }
+        }
     }
 }
