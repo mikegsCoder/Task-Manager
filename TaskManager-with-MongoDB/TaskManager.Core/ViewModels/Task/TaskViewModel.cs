0102f653757f0d4e23bd20ead06480cd069e0a62
mikegsCoder <mikegsid@gmail.com>
2023-06-18 11:43:43 +0300

    Add TaskViewModel

---
+++ TaskManager-with-MongoDB/TaskManager.Core/ViewModels/Task/TaskViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using TaskManager.Infrastructure.Data.Models.DataBaseModels;
+
+namespace TaskManager.Core.ViewModels.Task
+{
+    public class TaskViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string? Category { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime? CreatedOnDate { get; set; }
+
+        public string? CreatedOn 
+        { 
+            get => CreatedOnDate == null ? "" : CreatedOnDate.Value.ToString("dd/MM/yyyy");
+        }
+
+        public bool IsFinished { get => FinishedOnDate != null; }
+
+        public DateTime? FinishedOnDate { get; set; }
+
+        public string? FinishedOn 
+        {
+            get => FinishedOnDate == null ? "" : FinishedOnDate.Value.ToString("dd/MM/yyyy");
+        }
+    }
+}
