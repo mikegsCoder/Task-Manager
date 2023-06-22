1de75cad79bff8e65aa32b2769ae8953d966b865
mikegsCoder <mikegsid@gmail.com>
2023-06-22 16:21:06 +0300

    Add TaskService into ServiceCollectionExtension

--- TaskManager-with-MongoDB/TaskManager.WPF/Extensions/TaskManagerServiceCollectionExtension.cs
+++ TaskManager-with-MongoDB/TaskManager.WPF/Extensions/TaskManagerServiceCollectionExtension.cs
@@ -3,6 +3,7 @@ using TaskManager.WPF.DataContexts;
 using MongoDB.Driver;
 using TaskManager.Core.Services.UserService;
 using TaskManager.WPF.Controllers;
+using TaskManager.Core.Services.Task;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -16,6 +17,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<MainWindowContext>();
             services.AddSingleton(typeof(IMongoClient), new MongoClient("mongodb://localhost:27017"));
             services.AddScoped(typeof(IUserService), typeof(UserService));
+            services.AddScoped(typeof(ITaskService), typeof(TaskService));
 
             return services;
         }
