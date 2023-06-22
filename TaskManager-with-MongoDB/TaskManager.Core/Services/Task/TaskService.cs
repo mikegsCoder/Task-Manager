cd44ab512d7cc47d48beffd186c7f5b1f3738f5c
mikegsCoder <mikegsid@gmail.com>
2023-06-22 16:19:13 +0300

    Add TaskService

---
+++ TaskManager-with-MongoDB/TaskManager.Core/Services/Task/TaskService.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using TaskManager.Core.ViewModels.Task;
+using TaskManager.Infrastructure.Data;
+using TaskManager.Infrastructure.Data.Models.DataBaseModels;
+using MongoDB.Driver;
+using Microsoft.VisualBasic;
+using MongoDB.Bson;
+using System.Collections;
+using System.Data.Common;
+using TaskManager.Core.Constants;
+
+namespace TaskManager.Core.Services.Task
+{
+    public class TaskService : ITaskService
+    {
+        private readonly IMongoClient client;
+        private readonly IMongoDatabase db;
+
+        private IMongoCollection<User> userCollection;
+        private IMongoCollection<UserTask> taskCollection;
+        private IMongoCollection<Status> statusCollection;
+        private IMongoCollection<Category> categoryCollection;
+        private IMongoCollection<Remark> remarkCollection;
+
+        public TaskService(IMongoClient _client)
+        {
+            client = _client;
+
+            db = client.GetDatabase(DatabaseConstants.DatabaseName);
+
+            userCollection = db.GetCollection<User>(DatabaseConstants.UserCollectionName);
+            taskCollection = db.GetCollection<UserTask>(DatabaseConstants.TaskCollectionName);
+            statusCollection = db.GetCollection<Status>(DatabaseConstants.StatusCollectionName);
+            categoryCollection = db.GetCollection<Category>(DatabaseConstants.CategoryCollectionName);
+            remarkCollection = db.GetCollection<Remark>(DatabaseConstants.RemarkCollectionName);
+        }
+
+        public async Task<bool> CreateTaskAsync(string userId, string description, string category)
+        {
+
+            return true;
+        }
+    }
+}
---
+++ TaskManager-with-MongoDB/TaskManager.Core/ViewModels/Remark/RemarkViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManager.Core.ViewModels.Remark
+{
+    public class RemarkViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string TaskId { get; set; } = null!;
+
+        public string? Content { get; set; }
+
+        public DateTime? CreatedOnDate { get; set; }
+
+        public string? CreatedOn
+        {
+            get => CreatedOnDate == null ? "" : CreatedOnDate.Value.ToString("dd/MM/yyyy");
+        }
+    }
+}
