8f5651af2401c5f460a7fb9914f8f05798c59570
mikegsCoder <mikegsid@gmail.com>
2023-06-23 18:31:39 +0300

    Add TaskController

---
+++ TaskManager-with-MongoDB/TaskManager.WPF/Controllers/TaskController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TaskManager.Core.Services.Task;
+using TaskManager.Core.Services.UserService;
+using TaskManager.Core.ViewModels.Remark;
+using TaskManager.Core.ViewModels.Task;
+using TaskManager.Core.ViewModels.User;
+using TaskManager.Infrastructure.Data.Models.DataBaseModels;
+using TaskManager.WPF.DataContexts;
+using static TaskManager.WPF.Windows.MessageBoxes.MessageBoxes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using TaskManager.Core.Constants;
+
+namespace TaskManager.WPF.Controllers
+{
+    public class TaskController
+    {
+        private readonly ITaskService taskService;
+        private readonly MainWindowContext context;
+
+        public TaskController(
+            ITaskService _taskService,
+            MainWindowContext _context)
+        {
+            taskService = _taskService;
+            context = _context;
+        }
+    }
+}
